45775185e97ef7ad558a198fcb87761dd29c3298
renatNoore <renat.noore@gmail.com>
2021-01-08 09:06:05 +0300

    some

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
         alt="jocker" 
         className="absolute object-cover w-full h-full"/>
         <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-            <h1 className="text-6xl text-red-300 font-bold cursive leading-none lg:leading-snug home-name">Hi. I'm Rezzakk</h1>
+            <h2 className="text-4xl text-blue-800 font-bold cursive leading-none lg:leading-snug home-name">Hi. I'm Rezzakk</h2>
         </section>
     </main>)
 }
